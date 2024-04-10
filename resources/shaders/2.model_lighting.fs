84aca8ae30ebe2a66253df1d40ca2b3bc551d715
Sofija001 <128833511+Sofija001@users.noreply.github.com>
2024-04-10 22:39:02 +0200

    added blinn phong lighting

--- resources/shaders/2.model_lighting.fs
+++ resources/shaders/2.model_lighting.fs
@@ -63,9 +63,12 @@ vec3 CalcDirectionalLight(DirLight light, vec3 normal, vec3 viewDir)
     vec3 lightDir = normalize(-light.direction);
     // diffuse shading
     float diff = max(dot(normal, lightDir), 0.0);
-    // specular shading
-    vec3 reflectDir = reflect(-lightDir, normal);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+//     // specular shading
+//     vec3 reflectDir = reflect(-lightDir, normal);
+//     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+   // specular shading blinn phong
+    vec3 halfwayDir = normalize(lightDir + viewDir);
+    float spec = pow(max(dot(normal, halfwayDir), 0.0), material.shininess * 4);
     // combine results
     vec3 ambient = light.ambient * vec3(texture(material.texture_diffuse1, TexCoords));
     vec3 diffuse = light.diffuse * diff * vec3(texture(material.texture_diffuse1, TexCoords));
